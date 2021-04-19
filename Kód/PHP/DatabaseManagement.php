df17563d73a4a43590c7ad832e5c1b9bebe90c8a
Nagy Milán <nagymilan1008@gmail.com>
2021-04-19 19:50:12 +0200

    getConnection függvény befejezve
    #49

--- Kód/PHP/DatabaseManagement.php
+++ Kód/PHP/DatabaseManagement.php
@@ -3,11 +3,19 @@ Class DataBase{
   var $servername = "localhost";
   var $username = "root";
   var $password = "";
-  var $dbname = "rest";
+  var $dbname = "cinema_database";
   var $conn;
 
   function getConnstring() {
-    $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error()); 
+    $con = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname) or die("Connection failed: " . mysqli_connect_error());
+      
+    if (mysqli_connect_errno()) {
+       printf("Connect failed: %s\n", mysqli_connect_error());
+       exit();
+    } else {
+       $this->conn = $con;
+      }
+      return $this->conn;
+    }
   }
-}
 ?>
