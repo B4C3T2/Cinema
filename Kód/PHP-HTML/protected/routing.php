178907def80ee808ab3ccc1fca794a31041fbca7
Heim József <mrszenya@gmail.com>
2021-04-28 15:50:26 +0200

    PHP - routing.php - foglalással kapcsolatos oldalak meghívása

--- Kód/PHP-HTML/protected/routing.php
+++ Kód/PHP-HTML/protected/routing.php
@@ -5,6 +5,10 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 switch ($_GET['P']) {
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 
+	case 'book': IsUserLoggedIn() ? require_once PROTECTED_DIR.'booking/book.php' : header('Location: index.php'); break;
+
+	case 'refuse': IsUserLoggedIn() ? require_once PROTECTED_DIR.'booking/refuse.php' : header('Location: index.php'); break;
+	
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
 }
 
