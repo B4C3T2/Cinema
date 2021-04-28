b815a354ef9abc58017ad1336379d51c0205d2fb
WTI59Y <mano201611@gmail.com>
2021-04-28 16:52:45 +0200

    PHP/HTML-refuse.php oldal megírása

--- Kód/PHP-HTML/protected/booking/refuse.php
+++ Kód/PHP-HTML/protected/booking/refuse.php
@@ -1,4 +1,3 @@
-
 <?php
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['refuse'])) {
 	$postData = [
@@ -19,3 +18,40 @@
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Hyper Cinema - Lemondás</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbars/">
+  <!-- Saját CSS -->
+  <link rel="stylesheet" type="text/css" href="../../public/bejelentkezes_style.css">
+  <link rel="shortcut icon" href="../../public/pictures/icon.png"/>
+</head>
+<body>
+  <div>
+    <header>Lemondás<hr></header>  
+    <nav>
+      <div class="login-wrapper">
+        <form action="" class="form">
+          <table>
+            <tr>
+              <td>Fogalalás azonosító:</td>
+              <td><input type="text" name="bookingId" id="bookingId" maxlength="20" required></td>
+            </tr>
+            <tr>
+              <td class="bejelgomb">
+                <button type="submit"  value="lemondas" class="btn btn-outline-secondary">
+                  <a href ="home.html">Lemondás</a>
+                </button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
+    </nav>
+  </div>
+</body>
+</html>
