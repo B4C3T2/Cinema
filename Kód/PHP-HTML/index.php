9f1d4fa6e49462b917dff5d7f66c855ebec7e92a
Vajda Krisztián <uzleti02@gmail.com>
2021-04-29 00:47:56 +0200

    PHP/HTML - Index.php link javítva
    Rossz css-re mutatott illetve a userManager sem volt linkelve.

--- Kód/PHP-HTML/index.php
+++ Kód/PHP-HTML/index.php
@@ -10,7 +10,7 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<!-- Saját CSS -->
-	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'style.css?'.date('YmdHis', filemtime(PUBLIC_DIR.'style.css'))?>">
+	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'home_style.css?'.date('YmdHis', filemtime(PUBLIC_DIR.'home_style.css'))?>">
 </head>
 <body>
 	<div class="container-fluid">
